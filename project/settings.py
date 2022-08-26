2b7a342089e24605d27f0960b7174d634a0a86d6
Behzod <behzodme@gmail.com>
2022-08-26 16:23:36 +0500

    behzod

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(0cji5$u1*unhvw0tq2u$@ie*+)05avo9m#^!(x$ube170nl87'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
