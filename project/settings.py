1630c98a103a5bfd21b4bd00e4f75043de627cce
Behzod <behzodme@gmail.com>
2022-08-26 16:22:21 +0500

    behzod

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(0cji5$u1*unhvw0tq2u$@ie*+)05avo9m#^!(x$ube170nl87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
